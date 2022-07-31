0898828b1058958ff839e6529413f666561f573d
Abuzar Ansari <abuzarcs.email@gmail.com>
2022-08-01 00:31:14 +0530

    Subtract commited

--- LiveClass6/src/LiveClass/Subt.java
+++ LiveClass6/src/LiveClass/Subt.java
@@ -2,12 +2,14 @@ package LiveClass;
 
 public class Subt {
 
-	public Subt() {
-		// TODO Auto-generated constructor stub
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=20;
+		System.out.println(b-a);
+
 
 	}
 
