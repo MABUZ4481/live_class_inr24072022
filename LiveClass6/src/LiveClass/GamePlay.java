d5d2b97fa03b05899c96e9fb135099d672e1217c
Abuzar Ansari <abuzarcs.email@gmail.com>
2022-08-01 23:45:44 +0530

    Class of guesser, player and umpire added

--- LiveClass6/src/LiveClass/GamePlay.java
+++ LiveClass6/src/LiveClass/GamePlay.java
@@ -6,32 +6,58 @@ class GuesserGuess{
 	
 	int guesserNumber;
 	
-	Scanner sc=new Scanner(System.in);
-	public void guesserNum() {
-			
+	
+	public int guesserNum() {
+		Scanner sc=new Scanner(System.in);
 	guesserNumber = sc.nextInt();
 	System.out.println("Hi Guesser ! Please Guess any Number");
-	guesserNumber=sc.nextInt();		
+	guesserNumber=sc.nextInt();	
+	return guesserNumber;
+	
 	}	
 }
 class PlayersGuess{
 	
-	int fstPlayerNum,scndPlayerNum1,thdPlayerGuess;
+	int playerNum,scndPlayerNum1,thdPlayerGuess;
 	
-	Scanner sc=new Scanner(System.in);
-	public void playerGuess() {
+	
+	
+	public int playerGuess() {
+		Scanner sc=new Scanner(System.in);
 		System.out.println("Hi Player ! Please Guess any Number -1");
-		fstPlayerNum=sc.nextInt();
-		System.out.println("Hi Player ! Please Guess any Number -1");
-		scndPlayerNum1=sc.nextInt();
-		System.out.println("Hi Player ! Please Guess any Number -1");
-		thdPlayerGuess=sc.nextInt();
+		playerNum=sc.nextInt();		
+		return playerNum;
 	}
 	
+
 }
 
 class UmpireDecision{
 	
+	int keyValue;
+	int f1Player, f2Player,f3Player;
+	public void umpireTask() {
+		//System.out.println(" Hi all ! Welcome, Play now");
+	GuesserGuess gsr=new GuesserGuess();
+	//gsr.guesserNum();
+	keyValue=gsr.guesserNumber;
+	PlayersGuess pgs=new PlayersGuess();
+	//pgs.playerGuess();
+	
+	if(keyValue==pgs.playerNum) {
+		
+		System.out.println("Weldone Player-I ! You won the Game " );
+	}
+	else if (keyValue==pgs.scndPlayerNum1) {
+		
+		System.out.println("Weldone Player-II ! You won the Game " );
+	}
+	else if(keyValue==pgs.thdPlayerGuess) {
+		
+		System.out.println("Weldone Player-III ! You won the Game");
+	}
+	System.out.println("Oh!!! All lost the Game");
+	}
 	
 }
 
@@ -39,7 +65,12 @@ public class GamePlay {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(" Hi all ! Welcome in Number  Guess Game");
+		GuesserGuess gs=new GuesserGuess();
+		gs.guesserNum();
+		PlayersGuess pgs=new PlayersGuess();
+		pgs.playerGuess();
+		UmpireDecision umpire = new UmpireDecision();
+		umpire.umpireTask();
 	}
-
 }
