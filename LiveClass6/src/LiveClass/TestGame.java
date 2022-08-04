e34b150ea2562e33a615f0dd1daed702cdc9de53
Abuzar Ansari <77245761+MABUZ4481@users.noreply.github.com>
2022-08-04 12:37:27 +0100

    Taking input of five player using array fixed and input number validation check done

--- LiveClass6/src/LiveClass/TestGame.java
+++ LiveClass6/src/LiveClass/TestGame.java
@@ -22,36 +22,60 @@ class Parts{
 		return x;
 	}
 }
+class GuesserGues{
+	
+	double guesserKey;
+	Scanner sc=new Scanner(System.in);
+	public double guesserNumber(){
+		
+				
+	 guesserKey=sc.nextDouble();
+		
+		if(guesserKey%1!=0) {
+			System.out.println("Decimal Number is not Allowed. Please Enter the Integere Number Only");
+			guesserNumber();
+			
+		}
+		else {
+		return guesserKey;
+		}
+		return guesserKey;
+	}
+}
 
  class Players{
 	
-	public void countPlayers(int xt){
+	public double[] countPlayers(int xt){
 		
 		Scanner sc=new Scanner(System.in);
 		
 		double [] arr= new double[xt];
 		for(int i=0;i<arr.length;i++) {
 			
-			System.out.println("Please Enter Number " );
+			//System.out.println("Please Enter Number " );
 			arr[i]=sc.nextDouble();
 			if(arr[i]%1!=0) {
 				System.out.println("Decimal number is not allowed. Please re-enter");
 				countPlayers(xt);
 			}
 			else {
-				System.out.println("Valid number");
+			
+				System.out.println("Please Enter Number next " );
 			}
+			
 		}
 		
-      for(int j=0;j<arr.length;j++) {
+          for(int j=0;j<arr.length;j++) {
 			
 			System.out.println("Player-"+(j+1)+": " +(int)arr[j]);
 			
 		}
+          return arr;
 	}
 		
 		
 }
+ 
 
 
 class TestGame{
@@ -61,8 +85,12 @@ class TestGame{
 		System.out.println("Please enter the number of participants");
 		Parts pt=new Parts();
 		int k=pt.totalx();
+		GuesserGues gnum=new GuesserGues();
+		System.out.println("Guesser Please enter the number");
+		gnum.guesserNumber();
 		Players pn=new Players();
 		//int k=pt.totalx();
+		System.out.println("Participants Please enter the number");
 		pn.countPlayers(k);
 		System.exit(0);
 	}
