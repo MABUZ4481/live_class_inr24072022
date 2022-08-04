e89c2b4c9300fa03b529c4fe211ee86ab1d51ce4
Abuzar Ansari <77245761+MABUZ4481@users.noreply.github.com>
2022-08-04 11:54:44 +0100

    No. validation has been added

--- LiveClass6/src/LiveClass/TestGame.java
+++ LiveClass6/src/LiveClass/TestGame.java
@@ -27,17 +27,31 @@ class Parts{
 	
 	public void countPlayers(int xt){
 		
-		Parts pn=new Parts();
-		//xt=pn.x;
-		int [] arr= new int[xt];
+		Scanner sc=new Scanner(System.in);
+		
+		double [] arr= new double[xt];
 		for(int i=0;i<arr.length;i++) {
 			
-			System.out.println("length: " +i);
-		}
+			System.out.println("Please Enter Number " );
+			arr[i]=sc.nextDouble();
+			if(arr[i]%1!=0) {
+				System.out.println("Decimal number is not allowed. Please re-enter");
+				countPlayers(xt);
+			}
+			else {
+				System.out.println("Valid number");
+			}
 		}
 		
-		
+      for(int j=0;j<arr.length;j++) {
+			
+			System.out.println("Player-"+(j+1)+": " +(int)arr[j]);
+			
+		}
 	}
+		
+		
+}
 
 
 class TestGame{
@@ -50,5 +64,6 @@ class TestGame{
 		Players pn=new Players();
 		//int k=pt.totalx();
 		pn.countPlayers(k);
+		System.exit(0);
 	}
 }
